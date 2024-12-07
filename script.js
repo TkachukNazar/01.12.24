d3cb85be6d6a930926ec1f43b269c09ffce7868e
TkachukNazar <tkachnazar931@gmail.com>
2024-12-07 13:54:00 +0200

    added files

---
+++ script.js
@@ -0,0 +1,11 @@
+let basket = [];
+basket.push("Молоко");
+basket.push("Хліб");
+basket.push("Кола");
+basket.push("Сир");
+basket.push("Печиво");
+while (basket.length > 0) {
+  alert(basket);
+  alert(basket.pop());
+}
+alert("Візок пустий");
